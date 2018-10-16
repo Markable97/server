a3df531b940a89f141ab688e90e69d247b152832
Mark Glushko <markableglushko@gmail.com>
2018-10-16 20:13:00 +0300

    запрос для всех матчей по каманде

--- src/DataBaseQuery.java
+++ src/DataBaseQuery.java
@@ -28,16 +28,19 @@ public class DataBaseQuery {
     private static PreparedStatement prepStateResultsPrevMatches;
     private static PreparedStatement prepStateCalendarNextMatches;
     private static PreparedStatement prepStatePlayerInfo;
+    private static PreparedStatement prepStateAllMatchesTeam;
     
     private static ResultSet resultTournamnetTable; 
     private static ResultSet resultsPrevMatches; 
     private static ResultSet calendarNextMatches;
     private static ResultSet resultPlayerInfo;
+    private static ResultSet resultAllMatches;
     
     private static ArrayList<TournamentTable> tournamentArray = new ArrayList<TournamentTable>();
     private static ArrayList<PrevMatches> prevMatchesArray = new ArrayList<>();
     private static ArrayList<NextMatches> nextMatchesArray = new ArrayList<>();
     private static ArrayList<Player> playerArray = new ArrayList<>();
+    private static ArrayList<PrevAllMatchesForTeam> allMatchesTeamArray = new ArrayList<>();
     //private static String outputQuery = "";
 
    
@@ -68,6 +71,9 @@ public class DataBaseQuery {
     ArrayList<Player> getPlayerArray(){
         return DataBaseQuery.playerArray;
     }
+    ArrayList<PrevAllMatchesForTeam> getAllMatches(){
+        return DataBaseQuery.allMatchesTeamArray;
+    }
     String getQueryOutput(){
         return queryOutput;
     }
@@ -80,49 +86,62 @@ public class DataBaseQuery {
             statment = connect.createStatement();
             //results = statment.executeQuery(query);
             prepStateTable = connect.prepareStatement("SELECT  name_division, team_name, games,point,wins,draws,losses,goals_scored,goals_conceded,logo\n" +
-"FROM footbal_database.tournament_table j\n" +
-"join divisions d on id_division = j.divisions_id_division\n" +
-"join teams t on id_team = teams_id_team\n" +
-"where j.divisions_id_division = ?\n" +
-"order by point desc;");
+                    "FROM footbal_database.tournament_table j\n" +
+                    "join divisions d on id_division = j.divisions_id_division\n" +
+                    "join teams t on id_team = teams_id_team\n" +
+                    "where j.divisions_id_division = ?\n" +
+                    "order by point desc;");
             prepStateTable.setInt(1, qDiv);
             resultTournamnetTable = prepStateTable.executeQuery();
             //Teams team;
             tournamentTableByDivision(resultTournamnetTable);//
             
             prepStateResultsPrevMatches = connect.prepareStatement("SELECT name_division,id_tour,h.team_name,goal_home,goal_visitor, g.team_name, date\n" +
-"FROM footbal_database.matches m\n" +
-"join teams h on teams_id_teamHome = h.id_team\n" +
-"join teams g on teams_id_teamVisitor = g.id_team\n" +
-"join divisions d on m.divisions_id_division = id_division\n" +
-"where  (to_days(curdate()) - to_days(date) ) >= 0 and (to_days(curdate()) - to_days(date)) < 8 and m.divisions_id_division = ?\n" +
-"order by date;");
+                    "FROM footbal_database.matches m\n" +
+                    "join teams h on teams_id_teamHome = h.id_team\n" +
+                    "join teams g on teams_id_teamVisitor = g.id_team\n" +
+                    "join divisions d on m.divisions_id_division = id_division\n" +
+                    "where  (to_days(curdate()) - to_days(date) ) >= 0 and (to_days(curdate()) - to_days(date)) < 8 and m.divisions_id_division = ?\n" +
+                    "order by date;");
             prepStateResultsPrevMatches.setInt(1, qDiv);
             resultsPrevMatches = prepStateResultsPrevMatches.executeQuery();
             resultsPrevMatchesByDivision(resultsPrevMatches);
             
             prepStateCalendarNextMatches = connect.prepareStatement("SELECT  name_division, id_tour,h.team_name, g.team_name, DATE_FORMAT((date), \n" +
-"CONCAT(' %d, ', ELT( MONTH((date)), 'Янв.','Фев.','Мар.','Апр.','Май.','Июн.','Июл.','Авг.','Сен.','Окт.','Ноя.','Дек.'),' %H:%i')), name_stadium\n" +
-"FROM footbal_database.matches m\n" +
-"join teams h on teams_id_teamHome = h.id_team\n" +
-"join teams g on teams_id_teamVisitor = g.id_team\n" +
-"join stadiums s on stadiums_id_stadium = s.id_stadium\n" +
-"join divisions d on m.divisions_id_division = id_division\n" +
-"where date >= curdate() and m.divisions_id_division = ?\n" +
-"order by date;");
+                    "CONCAT(' %d, ', ELT( MONTH((date)), 'Янв.','Фев.','Мар.','Апр.','Май.','Июн.','Июл.','Авг.','Сен.','Окт.','Ноя.','Дек.'),' %H:%i')), name_stadium\n" +
+                    "FROM footbal_database.matches m\n" +
+                    "join teams h on teams_id_teamHome = h.id_team\n" +
+                    "join teams g on teams_id_teamVisitor = g.id_team\n" +
+                    "join stadiums s on stadiums_id_stadium = s.id_stadium\n" +
+                    "join divisions d on m.divisions_id_division = id_division\n" +
+                    "where date >= curdate() and m.divisions_id_division = ?\n" +
+                    "order by date;");
             prepStateCalendarNextMatches.setInt(1,qDiv);     
             calendarNextMatches = prepStateCalendarNextMatches.executeQuery();
             calendarResults(calendarNextMatches);
             
             prepStatePlayerInfo = connect.prepareStatement("SELECT id_player, team_name, name, name_amplua, birthdate, number, games, goal, assist, yellow_card, red_card, photo \n" +
-"FROM footbal_database.players p\n" +
-"join amplua a on id_amplua = p.amplua_id_amplua\n" +
-"join teams t on id_team = p.teams_id_team\n" +
-"join players_statistics ps on ps.players_id_player = p.id_player\n" +
-"where team_name like ?;");
+                    "FROM footbal_database.players p\n" +
+                    "join amplua a on id_amplua = p.amplua_id_amplua\n" +
+                    "join teams t on id_team = p.teams_id_team\n" +
+                    "join players_statistics ps on ps.players_id_player = p.id_player\n" +
+                    "where team_name like ?;");
             prepStatePlayerInfo.setString(1, qTeam);
             resultPlayerInfo = prepStatePlayerInfo.executeQuery();
             playerInfo(resultPlayerInfo);
+            
+            prepStateAllMatchesTeam = connect.prepareStatement("SELECT name_division,id_tour,h.team_name,goal_home,"
+                    + "goal_visitor, g.team_name, h.logo, g.logo\n" +
+                    "FROM footbal_database.matches m \n" +
+                    "join teams h on teams_id_teamHome = h.id_team \n" +
+                    "join teams g on teams_id_teamVisitor = g.id_team\n" +
+                    "join divisions d on m.divisions_id_division = id_division\n" +
+                    "where h.team_name like ? or g.team_name like ?\n" +
+                    "order by id_tour desc;");
+            prepStateAllMatchesTeam.setString(1, qTeam);
+            prepStateAllMatchesTeam.setString(2, qTeam);
+            resultAllMatches = prepStateAllMatchesTeam.executeQuery();
+            allMatchesForTeam(resultAllMatches);
             /*while(results.next()){
                 int id = results.getInt("id_team");
                 String team_name = results.getString("team_name");
@@ -247,6 +266,26 @@ public class DataBaseQuery {
                         goal, assist, yellowCard, redCard, playerUrlImage));
             }
             System.out.println("[4]Вывод запроса из класса БД \n" + playerArray.toString());
+        } catch (SQLException ex) {
+            Logger.getLogger(DataBaseQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void allMatchesForTeam(ResultSet result){
+        prevMatchesArray.clear();
+        try {
+            while(result.next()){
+                String nameDivision = result.getString("name_division");
+                int idTour = result.getInt("id_tour");
+                String teamHome = result.getString("h.team_name");
+                int goalHome = result.getInt("goal_home");
+                int goalVisit = result.getInt("goal_visitor");
+                String teamVisit = result.getString("g.team_name");
+                String urlImageH = result.getString("h.logo");
+                String urlImageG = result.getString("g.logo");
+                allMatchesTeamArray.add(new PrevAllMatchesForTeam(nameDivision, idTour, teamHome, goalHome, goalVisit, teamVisit, urlImageH, urlImageG) );
+            }
+            System.out.println("[5]Вывод запроса из класса БД \n" + allMatchesTeamArray.toString());
         } catch (SQLException ex) {
             Logger.getLogger(DataBaseQuery.class.getName()).log(Level.SEVERE, null, ex);
         }
